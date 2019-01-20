efa03150b695f345f935e28dca5c490600fc4627
Santiago Palladino <spalladino@gmail.com>
2019-01-19 21:05:15 -0300

    Support legacy browsers

--- src/eth/network.js
+++ src/eth/network.js
@@ -4,12 +4,6 @@ let web3;
 
 export function getWeb3() {
   if (!web3) {
-    web3 = new Web3(Web3.givenProvider || process.env.REACT_APP_PROVIDER_URL);
-  }
-  return web3;
-}
-
-export async function getAccount() {
-  const accounts = await window.ethereum.enable();
-  return accounts[0];
+    web3 = new Web3(window.ethereum || (window.web3 && window.web3.currentProvider) || process.env.REACT_APP_PROVIDER_URL);
+  } return web3;
 }
